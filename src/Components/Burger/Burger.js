454dde95eb676aee3855627c15c603a35da08eac
unknown <joshisaurabh2610@gmail.com>
2021-02-25 16:20:37 +0530

    burger build

--- src/Components/Burger/Burger.js
+++ src/Components/Burger/Burger.js
@@ -1,14 +1,14 @@
 import React from 'react'
-import Aux from '../../hoc/Auxillary';
+// import Aux from '../../hoc/Auxillary';
 import BurgerIngredient from './BurgerIngredients/burgerIngredient';
 import classes from './Burger.module.css'
 const burger =()=>{
     return (
-      <Aux className={classes.burger}>
+      <div className={classes.burger}>
           <BurgerIngredient type="bread-top"/>
           <BurgerIngredient type="cheese"/>
           <BurgerIngredient type="bread-bottom"/>
-      </Aux>
+      </div>
     );
 }
 
